9fb0f3484ea1a9a1314bbc02f026a240ca21359b
muazislambabar <muazislambabar@gmail.com>
2026-09-14 03:17:15 +0500

    services section complete

--- Banker_theme/src/Servicetemp.jsx
+++ Banker_theme/src/Servicetemp.jsx
@@ -1,7 +1,20 @@
-function Servicestemp() {
-    return(
+function Servicestemp({ icon, title, description, link }) {
+  return (
+    <div className="card" style={{ width: "18rem" }}>
+      {/*link code line here*/}
+      <div className="card-body">
+        <h5 className="card-title">{title}</h5>
 
-    )
+        <p className="card-text">{description}</p>
+      </div>
+
+      <div className="card-body">
+        <a href={link} className="card-link">
+          Learn More
+        </a>
+      </div>
+    </div>
+  );
 }
 
 export default Servicestemp;
--- Banker_theme/src/section-7-services/Services.jsx
+++ Banker_theme/src/section-7-services/Services.jsx
@@ -1,8 +1,63 @@
+import Servicestemp from "../Servicetemp";
+
+const data = [
+  {
+    icon: "link",
+    title: "Business Consulting",
+    description:
+      "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    link: "df",
+  },
+  {
+    icon: "link",
+    title: "Business Consulting",
+    description:
+      "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    link: "df",
+  },
+  {
+    icon: "link",
+    title: "Business Consulting",
+    description:
+      "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    link: "df",
+  },
+  {
+    icon: "link",
+    title: "Business Consulting",
+    description:
+      "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    link: "df",
+  },
+  {
+    icon: "link",
+    title: "Business Consulting",
+    description:
+      "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    link: "df",
+  },
+  {
+    icon: "link",
+    title: "Business Consulting",
+    description:
+      "A small river named Duden flows by their place and supplies it with the necessary regelialia.",
+    link: "df",
+  },
+];
 function Services() {
   return (
-    <services>
-      <h1>Services Section</h1>
-    </services>
+    <section className="services">
+      <h1>Our Services</h1>
+
+      {data.map((element) => (
+        <div
+          key={element.icon}
+          className="col-12 col-md-6 col-lg-4 d-flex justify-content-center"
+        >
+          <Servicestemp {...element} />
+        </div>
+      ))}
+    </section>
   );
 }
 
